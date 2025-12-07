3ec135b3e21417409b96bee253c3a134fd079d3e
Gilbert marshal <gilbertmarshal2001@gmail.com>
2025-12-07 18:14:12 +0530

    Update sumofdigit.py

--- day2/sumofdigit.py
+++ day2/sumofdigit.py
@@ -1,1 +1,21 @@
+#Sum of numbers in a iteration
+
+"""num= int(input("Enter the number: "))
+count=0
+for i in range(1,num+1):
+    count+=i
+print(count)"""
+
+#Sum of digits in a number
+
+num=12345
+total=0
+while num >0:
+    digit = num%10
+    total=total+digit
+    num//=10
+print(total)
+
+
+
 
